21291468b4df1241678033132dd882a1773eda9e
inchwara <inchwara@gmail.com>
2017-02-10 14:47:00 +0300

    add and view funtions

--- learning_progress.py
+++ learning_progress.py
@@ -2,10 +2,43 @@
 This is a commandline application for tracking learning progress. Made with love by TeamAwesome
 Usage:
     learning_map add  <code>
-    learning_map view <port> 
-    learning_map 
-    learning_map 
+    learning_map view [--studied] [--todo]
+    learning_map studied
+    learning_map not_studied
 
 
 """
 
+import sys
+import cmd
+from docopt import docopt
+
+
+class LearningTracker (cmd.Cmd):
+    intro = 'Add Skill'
+    prompt = '(learning_map) '
+
+
+    def do_add(self, arg):
+        """Usage: add <code> """
+
+        print(arg)
+
+    def do_view(self, arg):
+        """Usage: view <port>
+
+        """
+
+        print(arg)
+
+    def do_quit(self, arg):
+
+        print('See you next time!')
+        exit()
+
+opt = docopt(__doc__, sys.argv[1:])
+
+if opt['--tracker']:
+    LearningTracker().cmdloop()
+
+print(opt)
