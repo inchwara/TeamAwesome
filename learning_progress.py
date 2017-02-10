e975aeeddd3f8155577967a59799fda15f056331
LZRS <lazloti@gmail.com>
2017-02-10 14:47:10 +0300

    edit doc...

--- learning_progress.py
+++ learning_progress.py
@@ -1,11 +1,11 @@
 """
 This is a commandline application for tracking learning progress. Made with love by TeamAwesome
+
 Usage:
     learning_map add  <code>
-    learning_map view <port> 
-    learning_map 
-    learning_map 
+    learning_map view [--studied] [--todo] 
+    learning_map show-progress
+    learning_map (-h | --help)
 
 
 """
-
